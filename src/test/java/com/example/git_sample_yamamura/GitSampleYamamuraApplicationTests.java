037c52017911c61d929d2b3433f5063d148c829d
suinmi24 <yjadeone@gmail.com>
2025-05-19 11:41:14 +0900

    src/test/java/com/example/git_sample_yamamura/GitSampleYamamuraApplicationTests.jav#

---
+++ src/test/java/com/example/git_sample_yamamura/GitSampleYamamuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_sample_yamamura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleYamamuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
